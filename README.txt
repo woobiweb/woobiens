4ab52f4d7aab6b54c38990be1c4f9516f724d25e
Woobiweb <brunoitaliano.developer@gmail.com>
2021-03-04 20:28:51 +0100

    First commit

---
+++ README.txt
@@ -0,0 +1,5 @@
+Bootstrap 4 - Barrio SASS Starter Kit
+
+Branch master: installazione base
+Branch dev: woobiweb customizzazione base
+Brach bienestarte: versione bienestarte
---
+++ js/custom.js
@@ -0,0 +1,22 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.woobienes = {
+    attach: function(context, settings) {
+
+      // Custom code here
+
+    }
+  };
+
+  $('#collapseLeggere').collapse({
+    toggle: false
+  })
+
+})(jQuery, Drupal);
