0a63df4f12ca8697c72aded0db5e5be40b1dd1c9
Woobiweb <brunoitaliano.developer@gmail.com>
2021-03-30 16:11:39 +0200

    Attivazione Main Menu con modifica via JS

--- js/custom.js
+++ js/custom.js
@@ -20,3 +20,25 @@
   })
 
 })(jQuery, Drupal);
+
+//////////////////////// JAVASCRIPT
+'use strict';
+
+const homeLink = document.getElementsByClassName('nav__link');
+
+/*
+change Home link with Opita Link
+add opita class to home
+ */
+
+// console.log(homeLink);
+for (const link of homeLink) {
+  const att = link.getAttribute('data-drupal-link-system-path');
+  if (att === '<front>') {
+    link.innerHTML = 'OPITA';
+    link.setAttribute('class', 'nav__link opita')
+  }
+}
+// console.log(menu);
+
+
